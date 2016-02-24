a76d573df5ef4e4f3a3c8ed0b9d784ae36657c9f
Zabed Akbar <zabed.akbar@gmail.com>
2016-02-24 14:08:08 -0700

    Added Generic Node Class

---
+++ CSharp/CSharp/TreeGraphs/Node.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.TreeGraphs
+{
+    public class Node<T>
+    {
+        private T data;
+        private NodeList<T> neighbors = null;
+
+        public Node() { }
+        public Node(T data) : this(data, null) { }
+        public Node(T data, NodeList<T> neighbors)
+        {
+            this.data = data;
+            this.neighbors = neighbors;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+
+        protected NodeList<T> Neighbors
+        {
+            get
+            {
+                return neighbors;
+            }
+            set
+            {
+                neighbors = value;
+            }
+        }
+    }
+
+    public class NodeList<T> : Collection<Node<T>>
+    {
+        public NodeList() : base() { }
+
+        public NodeList(int initialSize)
+        {
+            for (int i = 0; i < initialSize; initialSize++)
+            {
+                base.Items.Add(default(Node<T>));
+            }
+        }
+
+        public Node<T> FindByValue(T value)
+        {
+            foreach (Node<T> node in Items)
+            {
+                if (node.Value.Equals(value))
+                {
+                    return node;
+                }
+
+            }
+            return null;
+        }
+    }
+}
---
+++ CSharp/CSharpUnitTests/NodeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpUnitTests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
